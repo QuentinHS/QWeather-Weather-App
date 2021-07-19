75c4e22e36648f4059d1df60608cbe449b9609a2
QuentinHS <quentin.halysummerfield@gmail.com>
2021-07-19 15:15:30 +1000

    final project upload

---
+++ scripts/forecast.js
@@ -0,0 +1,26 @@
+let key = '2r0HbGb8QvA1siDbeXHe8GrB8I85owDm'
+
+
+
+const getWeather = async (id) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query)
+    const data = await response.json()
+
+    return data[0]
+}
+
+// Get city information
+
+const getCity = async (city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'
+    const query = `?apikey=${key}&q=${city}`
+
+    const response = await fetch(base + query)
+    const data = await response.json()
+
+    return data[0]
+
+}
